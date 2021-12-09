4c83bc7b9e0d13a0c93f18e793603a2399a61873
Hady Achkar <hady.achkar@hotmail.com>
2021-12-09 20:07:28 +0200

    init funnels settings

--- src/components/common/FunnelSettings.tsx
+++ src/components/common/FunnelSettings.tsx
@@ -1,9 +1,64 @@
-import React from 'react'
+import React, {useCallback, useEffect} from 'react'
 import {Fragment, useState} from 'react'
-import {Dialog, Transition} from '@headlessui/react'
-import {CheckIcon} from '@heroicons/react/outline'
+import {Dialog, Transition, Listbox} from '@headlessui/react'
+import {CheckIcon, SelectorIcon} from '@heroicons/react/outline'
+import classNames from 'classnames'
+import {PublishTab, GeneralTab} from './SettingsTabs'
+import {useParams, useHistory} from 'react-router-dom'
+import {useSelector} from 'react-redux'
+import {AppState} from '../../reducers'
+import {IFunnel} from '../../types'
 
 const FunnelsSettings = ({open, setOpen}) => {
+	const {funnelTitle} = useParams()
+	const {funnels} = useSelector((state: AppState) => state.funnels)
+
+	const [funnelState, setFunnelState] = useState<IFunnel>()
+
+	const history = useHistory()
+	const fetchFunnel = useCallback(() => {
+		const funnel = funnels.find((funnel) => funnel.title === funnelTitle)
+		if (!funnel) {
+			history.push('/404')
+		} else {
+			setFunnelState(funnel)
+		}
+	}, [funnelTitle])
+	useEffect(() => {
+		fetchFunnel()
+		return () => {
+			fetchFunnel()
+		}
+	}, [funnelTitle])
+
+	console.log('funnel state:', funnelState)
+
+	const [tabs, setTabs] = useState([
+		{
+			name: 'General',
+			component: <GeneralTab setOpen={setOpen} open={open} />,
+			current: true,
+		},
+		{
+			name: 'Publish',
+			component: <PublishTab setOpen={setOpen} open={open} />,
+			current: false,
+		},
+		{name: 'Analytics', component: <div>hello 3</div>, current: false},
+		{name: 'Navigation', component: <div>hello 4</div>, current: false},
+	])
+
+	const handleToggleTabs = useCallback((index: number) => {
+		setTabs((prevState) => [
+			...prevState.map((item, i) => {
+				return index === i
+					? {...item, current: true}
+					: {...item, current: false}
+			}),
+		])
+	}, [])
+	console.log(tabs)
+
 	return (
 		<Transition.Root show={open} as={Fragment}>
 			<Dialog
@@ -41,155 +96,47 @@ const FunnelsSettings = ({open, setOpen}) => {
 						leaveTo="opacity-0 translate-y-4 sm:translate-y-0 sm:scale-95"
 					>
 						<div className="inline-block align-bottom bg-white rounded-lg px-4 pt-5 pb-4 text-left overflow-hidden shadow-xl transform transition-all sm:max-w-3xl sm:my-8 sm:align-middle sm:w-full sm:p-6">
-							<form className="space-y-8 divide-y divide-gray-200">
-								<div className="space-y-8 divide-y divide-gray-200 sm:space-y-5">
-									<div>
-										<div>
-											<h3 className="text-lg leading-6 font-medium text-gray-900">
-												Funnel Settings
-											</h3>
-											<p className="mt-1 max-w-2xl text-sm text-gray-500">
-												Edit the general funnel details
-											</p>
-										</div>
-
-										<div className="mt-6 sm:mt-5 space-y-6 sm:space-y-5">
-											<div className="sm:grid sm:grid-cols-3 sm:gap-4 sm:items-start sm:border-t sm:border-gray-200 sm:pt-5">
-												<label
-													htmlFor="title"
-													className="block text-sm font-medium text-gray-700 sm:mt-px sm:pt-2"
+							<div>
+								<div className="sm:hidden">
+									<label htmlFor="tabs" className="sr-only">
+										Select a tab
+									</label>
+									<select
+										id="tabs"
+										name="tabs"
+										className="block w-full focus:ring-indigo-500 focus:border-indigo-500 border-gray-300 rounded-md"
+										defaultValue={tabs[0].name}
+									>
+										{tabs.map((tab) => (
+											<option key={tab.name}>{tab.name}</option>
+										))}
+									</select>
+								</div>
+								<div className="hidden sm:block">
+									<div className="border-b border-gray-200">
+										<nav className="-mb-px flex" aria-label="Tabs">
+											{tabs.map((tab, index) => (
+												<button
+													key={tab.name}
+													onClick={() => handleToggleTabs(index)}
+													className={classNames(
+														tab.current
+															? 'border-b border-indigo-500 text-indigo-600'
+															: 'border-transparent text-gray-500 hover:text-gray-700 hover:border-gray-300',
+														'w-1/4 py-4 px-1 text-center border-b-2 font-medium text-sm'
+													)}
+													aria-current={tab.current ? 'page' : undefined}
 												>
-													Funnel title
-												</label>
-												<div className="mt-1 sm:mt-0 sm:col-span-2">
-													<div className="max-w-lg flex rounded-md shadow-sm">
-														<input
-															type="text"
-															name="title"
-															id="title"
-															autoComplete="title"
-															className="flex-1 block w-full focus:ring-indigo-500 focus:border-indigo-500 min-w-0 rounded sm:text-sm border-gray-300"
-														/>
-													</div>
-												</div>
-											</div>
-
-											<div className="sm:grid sm:grid-cols-3 sm:gap-4 sm:items-start sm:border-t sm:border-gray-200 sm:pt-5">
-												<label
-													htmlFor="email"
-													className="block text-sm font-medium text-gray-700 sm:mt-px sm:pt-2"
-												>
-													Contact email
-												</label>
-												<div className="mt-1 sm:mt-0 sm:col-span-2">
-													<div className="max-w-lg flex rounded-md shadow-sm">
-														<input
-															type="email"
-															name="email"
-															id="email"
-															autoComplete="email"
-															className="flex-1 block w-full focus:ring-indigo-500 focus:border-indigo-500 min-w-0 rounded sm:text-sm border-gray-300"
-														/>
-													</div>
-												</div>
-											</div>
-
-											<div className="sm:grid sm:grid-cols-3 sm:gap-4 sm:items-start sm:border-t sm:border-gray-200 sm:pt-5">
-												<label
-													htmlFor="meta"
-													className="block text-sm font-medium text-gray-700 sm:mt-px sm:pt-2"
-												>
-													Meta desctiption
-												</label>
-												<div className="mt-1 sm:mt-0 sm:col-span-2">
-													<textarea
-														id="meta"
-														name="meta"
-														rows={3}
-														className="max-w-lg shadow-sm block w-full focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm border border-gray-300 rounded-md"
-														defaultValue={''}
-													/>
-													<p className="mt-2 text-sm text-gray-500">
-														Meta description, recommended for SEO
-													</p>
-												</div>
-											</div>
-										</div>
-									</div>
-
-									<div className="pt-8 space-y-6 sm:pt-10 sm:space-y-5">
-										<div>
-											<h3 className="text-lg leading-6 font-medium text-gray-900">
-												Publish settings
-											</h3>
-											<p className="mt-1 max-w-2xl text-sm text-gray-500">
-												Add a domain name, or use our default
-											</p>
-										</div>
-										<div className="space-y-6 sm:space-y-5">
-											<div className="sm:grid sm:grid-cols-3 sm:gap-4 sm:items-start sm:border-t sm:border-gray-200 sm:pt-5">
-												<label
-													htmlFor="domain"
-													className="block text-sm font-medium text-gray-700 sm:mt-px sm:pt-2"
-												>
-													Domain name
-												</label>
-												<div className="mt-1 sm:mt-0 sm:col-span-2">
-													<div className="max-w-lg flex rounded-md shadow-sm">
-														<input
-															type="text"
-															name="domain"
-															id="domain"
-															autoComplete="domain"
-															className="flex-1 block w-full focus:ring-indigo-500 focus:border-indigo-500 min-w-0 rounded sm:text-sm border-gray-300"
-														/>
-													</div>
-												</div>
-											</div>
-
-											<div className="bg-white  sm:rounded-lg">
-												<div className="sm:grid sm:grid-cols-3 sm:gap-4 sm:items-start sm:border-t sm:border-gray-200 sm:pt-5">
-													<label className="block text-sm font-medium text-gray-700 sm:mt-px sm:pt-2">
-														Delete your funnel
-													</label>
-													<div className="mt-2 max-w-xl text-sm text-gray-500">
-														<p>
-															Once you delete your funnel, you will lose all
-															data associated with it.
-														</p>
-													</div>
-													<div className="mt-5">
-														<button
-															type="button"
-															className="inline-flex items-center justify-center px-4 py-2 border border-transparent font-medium rounded-md text-red-700 bg-red-100 hover:bg-red-200 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-red-500 sm:text-sm"
-														>
-															Delete account
-														</button>
-													</div>
-												</div>
-											</div>
-										</div>
+													{tab.name}
+												</button>
+											))}
+										</nav>
 									</div>
 								</div>
-
-								<div className="pt-5">
-									<div className="flex justify-end">
-										<button
-											type="button"
-											className="bg-white py-2 px-4 border border-gray-300 rounded-md shadow-sm text-sm font-medium text-gray-700 hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
-											onClick={() => setOpen(false)}
-										>
-											Cancel
-										</button>
-										<button
-											type="submit"
-											className="ml-3 inline-flex justify-center py-2 px-4 border border-transparent shadow-sm text-sm font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
-										>
-											Save
-										</button>
-									</div>
-								</div>
-							</form>
+							</div>
+							<div className="mt-5">
+								{tabs.find((tab) => tab.current).component}
+							</div>
 						</div>
 					</Transition.Child>
 				</div>
---
+++ src/constants/Categories.ts
@@ -0,0 +1,7 @@
+export const categories = [
+	{id: 1, name: 'Domestic companies'},
+	{id: 2, name: 'Barber shops'},
+	{id: 3, name: 'Malls and stores'},
+	{id: 4, name: 'Gym and spa'},
+	{id: 5, name: 'Clothing'},
+]
