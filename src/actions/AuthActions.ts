5753998e2eaa537a7d825c8a5ce5b24fd1727f17
Hady Achkar <hady.achkar@hotmail.com>
2021-12-17 04:27:41 +0200

    signup

--- src/actions/AuthActions.ts
+++ src/actions/AuthActions.ts
@@ -4,7 +4,7 @@ import {AppState} from '../reducers'
 import Cookies from 'universal-cookie'
 import {IUser} from '../models/IUser'
 import login from '../services/Login'
-import {googleLogin} from '../services'
+import {googleLogin, signup, SignupPayload} from '../services'
 import {LoginPayload} from '../services'
 
 const cookies = new Cookies()
@@ -20,7 +20,19 @@ export const startLogin = (payload: LoginPayload) => {
 	return (dispatch: Dispatch<AppActions> | any, _: () => AppState) => {
 		login(email, password)
 			.then((res) => {
-				const {token, fullName, email, _id, type, stripeId} = res?.data
+				const {
+					token,
+					fullName,
+					email,
+					_id,
+					type,
+					stripeId,
+					activeSubscription,
+					isTrialLegit,
+					subscriptions,
+					inTrial,
+					paymentMethods,
+				} = res?.data
 				if (token) {
 					cookies.set('token', token, {path: '/'})
 					cookies.set('fullName', fullName, {path: '/'})
@@ -36,7 +48,12 @@ export const startLogin = (payload: LoginPayload) => {
 							_id,
 							type,
 							stripeId,
-						}),
+							activeSubscription,
+							subscriptions,
+							inTrial,
+							isTrialLegit,
+							paymentMethods,
+						})
 					)
 				}
 			})
@@ -54,14 +71,27 @@ interface GoogleLoginPayload {
 	fname: string
 	lname: string
 	email: string
+	priceId?: string
 }
 
 export const startGoogleLogin = (payload: GoogleLoginPayload) => {
-	const {fname, lname, email} = payload
+	const {fname, lname, email, priceId} = payload
 	return (dispatch: Dispatch<AppActions> | any, _: () => AppState) => {
-		googleLogin(fname, lname, email)
+		googleLogin(fname, lname, email, priceId)
 			.then((res) => {
-				const {token, fullName, email, _id, type, stripeId} = res?.data
+				const {
+					token,
+					fullName,
+					email,
+					_id,
+					type,
+					stripeId,
+					inTrial,
+					isTrialLegit,
+					activeSubscription,
+					paymentMethods,
+					subscriptions,
+				} = res?.data
 				if (token) {
 					cookies.set('token', token, {path: '/'})
 					cookies.set('fullName', fullName, {path: '/'})
@@ -78,7 +108,64 @@ export const startGoogleLogin = (payload: GoogleLoginPayload) => {
 							_id,
 							type,
 							stripeId,
-						}),
+							inTrial,
+							isTrialLegit,
+							activeSubscription,
+							paymentMethods,
+							subscriptions,
+						})
+					)
+				}
+			})
+			.catch((err) => {
+				if (err.response) {
+					console.log(err.response.data)
+				} else {
+					console.log(err)
+				}
+			})
+	}
+}
+
+export const startSignup = (payload: SignupPayload) => {
+	const {email, password, priceId, fname, lname} = payload
+	return (dispatch: Dispatch<AppActions> | any, _: () => AppState) => {
+		signup(payload)
+			.then((res) => {
+				const {
+					token,
+					fullName,
+					email,
+					_id,
+					type,
+					stripeId,
+					activeSubscription,
+					isTrialLegit,
+					subscriptions,
+					inTrial,
+					paymentMethods,
+				} = res?.data
+				if (token) {
+					cookies.set('token', token, {path: '/'})
+					cookies.set('fullName', fullName, {path: '/'})
+					cookies.set('email', email, {path: '/'})
+					cookies.set('_id', _id, {path: '/'})
+					cookies.set('type', type, {path: '/'})
+					cookies.set('stripeId', stripeId, {path: '/'})
+					dispatch(
+						loginAction({
+							token,
+							fullName,
+							email,
+							_id,
+							type,
+							stripeId,
+							activeSubscription,
+							isTrialLegit,
+							subscriptions,
+							inTrial,
+							paymentMethods,
+						})
 					)
 				}
 			})
